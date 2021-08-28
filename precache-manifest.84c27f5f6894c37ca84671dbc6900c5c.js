8ebcfd978664c4c2d929823cb56eba703d11a87d
Gowtham.K <51522882+gowthamkaruna@users.noreply.github.com>
2021-08-28 16:12:46 +0530

    Updates

---
+++ precache-manifest.84c27f5f6894c37ca84671dbc6900c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ab093e325fe4ddad3ff372310a409e",
+    "url": "/gowthamkaruna/pomodoro.git/index.html"
+  },
+  {
+    "revision": "d9cf2a6031df9a3401d2",
+    "url": "/gowthamkaruna/pomodoro.git/static/css/main.7fa66090.chunk.css"
+  },
+  {
+    "revision": "99d86b416ef6b2cc7e7a",
+    "url": "/gowthamkaruna/pomodoro.git/static/js/2.186ddd41.chunk.js"
+  },
+  {
+    "revision": "d9cf2a6031df9a3401d2",
+    "url": "/gowthamkaruna/pomodoro.git/static/js/main.2f9938af.chunk.js"
+  },
+  {
+    "revision": "affb17e3f3e67433a152",
+    "url": "/gowthamkaruna/pomodoro.git/static/js/runtime~main.96c01e14.js"
+  }
+]);
